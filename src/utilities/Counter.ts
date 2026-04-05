2fcaab36662be158dbe4683d3cc6a440ec3d4f91
syswraith <syswraith@proton.me>
2026-04-05 17:04:05 +0530

    pythonic Counter (sort of)
    - counts frequency
    - counts percentage

---
+++ src/utilities/Counter.ts
@@ -0,0 +1,39 @@
+import type CounterMap from "../types/CounterMap.ts";
+
+export default class Counter {
+  iterable: Iterable<string | number>;
+  private map: CounterMap = new Map();
+  private percentMap: CounterMap = new Map();
+
+  constructor(iterable: Iterable<string | number>) {
+    this.iterable = iterable;
+
+    this.setMaps();
+  }
+
+  // generate the maps internally
+  private setMaps(): void {
+    for (let i of this.iterable) {
+      this.map.set(i, (this.map.get(i) ?? 0) + 1);
+    }
+
+    let total = [...this.map.values()].reduce((sum, number) => sum + number, 0);
+    let multiplier = 100 / total;
+
+    for (const [key, value] of this.map) {
+      this.percentMap.set(key, value * multiplier);
+    }
+  }
+
+  // return a copy
+  getMap(): CounterMap {
+    return new Map(this.map);
+  }
+
+  getPercentMap(): CounterMap {
+    return new Map(this.percentMap);
+  }
+}
+
+let counter = new Counter("hello");
+console.log(counter.getPercentMap());
